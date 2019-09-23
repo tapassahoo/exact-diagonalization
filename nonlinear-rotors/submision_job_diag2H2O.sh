0b104ca370e97788bf65b220bfa882264f09d714
Tapas Sahoo <tapascuchem@gmail.com>
2019-09-23 16:11:42 -0400

    feynman: Final version

--- nonlinear-rotors/submision_job_diag2H2O.sh
+++ nonlinear-rotors/submision_job_diag2H2O.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=diag.out
 #SBATCH --time=0-00:30
 
-#SBATCH --mem-per-cpu=2048mb
+#SBATCH --mem-per-cpu=4096mb
 #SBATCH --cpus-per-task=1
 export OMP_NUM_THREADS=1
 python diag2H2O.py 2 8
