5f3639b8a4840da0ee2944375144701b87df6497
Tapas Sahoo <tapascuchem@gmail.com>
2019-10-08 09:48:15 -0400

    scripts changed

--- nonlinear-rotors/submision_exact_energy_nonlinear_rotors.sh
+++ nonlinear-rotors/submision_exact_energy_nonlinear_rotors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=diag1
-#SBATCH --output=diag1.out
+#SBATCH --job-name=diag3
+#SBATCH --output=diag3.out
 #SBATCH --time=1-00:30
 
-#SBATCH --mem-per-cpu=100GB
+#SBATCH --mem-per-cpu=180GB
 #SBATCH --cpus-per-task=1
 export OMP_NUM_THREADS=1
-python exact_energy_nonlinear_rotors.py 2 10
+python exact_energy_nonlinear_rotors.py 4 6
