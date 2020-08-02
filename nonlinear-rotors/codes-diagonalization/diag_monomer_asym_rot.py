e794cb27add85493e30db60251ea51a4108c2480
Tapas Sahoo <tapascuchem@gmail.com>
2020-08-01 23:33:52 -0400

    gitignore and diag_monomer_asym.py

--- nonlinear-rotors/codes-diagonalization/diag_monomer_asym_rot.py
+++ nonlinear-rotors/codes-diagonalization/diag_monomer_asym_rot.py
@@ -8,7 +8,7 @@ import sys
 import math
 import numpy as np
 from scipy import linalg as LA
-import pot
+import qpot
 from scipy.sparse.linalg import eigs, eigsh
 
 def binom(n,k):
@@ -52,16 +52,23 @@ def littleD(ldJ,ldmp,ldm,ldtheta):
 	return dval*tempD
 
 if __name__ == '__main__':    
-	strength=float(sys.argv[1])
+	zCOM=float(sys.argv[1])
 	Jmax=int(sys.argv[2])
-	angleNum = int(sys.argv[3])
-	print("Jmax = ", Jmax, flush=True)
-	print("angleNum = ", angleNum, flush=True)
-	strFile = "-Field-Strength-"+str(strength)+"Kinv-N1-Jmax-"+str(Jmax)+"-grid-"+str(angleNum)+".txt"
-	
-	#print the normalization 
-	write_pot = False
+	size_theta = int(2*Jmax+3)
+	size_phi = int(2*(2*Jmax+1))
+
 	tol = 10e-8
+	#print the normalization 
+	io_write = False
+	pot_write = False
+	norm_check = False
+	if (io_write == True):
+		print("Jmax = ", Jmax, flush=True)
+		print(" Number of theta grids = ", size_theta, flush=True)
+		print(" Number of phi and chi grids = ", size_phi, flush=True)
+		sys.stdout.flush()
+	zCOM = '{:3.2f}'.format(zCOM)
+	strFile = "diag-2-p-H2O-one-rotor-fixed-cost1-jmax"+str(Jmax)+"-Rpt"+str(zCOM)+"Angstrom-grids-"+str(size_theta)+"-"+str(size_phi)+"-saved-basis.txt"
 
 	#The rotational A, B, C constants are indicated by Ah2o, Bh2o and Ch2o, respectively. The unit is cm^-1. 
 	Ah2o= 27.877 #cm-1 
@@ -72,20 +79,20 @@ if __name__ == '__main__':
 	Bh2o=Bh2o*CMRECIP2KL
 	Ch2o=Ch2o*CMRECIP2KL
 
-	thetaNum = int(angleNum+1)                                           
-	xGL,wGL = np.polynomial.legendre.leggauss(thetaNum)              
-	phixiGridPts = np.linspace(0,2*np.pi,angleNum, endpoint=False)  
-	dphixi = 2.*np.pi / angleNum                                 
-	print("|------------------------------------------------")
-	print("| A list of Gaussian quadrature points of Legendre polynomials - ")
-	print("")
-	print(xGL)
-	print("")
-	print("| A list of the corrsponding weights - ")
-	print("")
-	print(wGL)
-	print("|------------------------------------------------")
-	sys.stdout.flush()
+	xGL,wGL=np.polynomial.legendre.leggauss(size_theta)              
+	phixiGridPts=np.linspace(0,2*np.pi,size_phi,endpoint=False)  
+	dphixi=2.*np.pi/size_phi
+	if (io_write == True):
+		print("|------------------------------------------------")
+		print("| A list of Gaussian quadrature points of Legendre polynomials - ")
+		print("")
+		print(xGL)
+		print("")
+		print("| A list of the corrsponding weights - ")
+		print("")
+		print(wGL)
+		print("|------------------------------------------------")
+		sys.stdout.flush()
 
 	JKM = int(((2*Jmax+1)*(2*Jmax+2)*(2*Jmax+3)/6)) #JKM = "Sum[(2J+1)**2,{J,0,Jmax}]" is computed in mathematica
 
@@ -96,16 +103,17 @@ if __name__ == '__main__':
 		JKoM = int((JKM+Jmax+1)/2)
 		JKeM = int(JKM-JKoM)
 
-	print("|------------------------------------------------")
-	print("| Number of basis functions calculations ....")
-	print("| ")
-	print("| # of |JKM> basis = "+str(JKM))
-	print("| ")
-	print("| # of even K in |JKM> = "+str(JKeM))
-	print("| # of odd  K in |JKM> = "+str(JKoM))
-	print("| ")
-	print("|------------------------------------------------")
-    
+	if (io_write == True):
+		print("|------------------------------------------------")
+		print("| Number of basis functions calculations ....")
+		print("| ")
+		print("| # of |JKM> basis = "+str(JKM))
+		print("| ")
+		print("| # of even K in |JKM> = "+str(JKeM))
+		print("| # of odd  K in |JKM> = "+str(JKoM))
+		print("| ")
+		print("|------------------------------------------------")
+		
 	JKeMQuantumNumList = np.zeros((JKeM,3),int)
 
 	#even
@@ -134,9 +142,9 @@ if __name__ == '__main__':
 	"""
 
 	#
-	dJKeM = np.zeros((JKeM,len(xGL)),float)
-	KJKeM = np.zeros((JKeM,angleNum),complex)
-	MJKeM = np.zeros((JKeM,angleNum),complex)
+	dJKeM = np.zeros((JKeM,size_theta),float)
+	KJKeM = np.zeros((JKeM,size_phi),complex)
+	MJKeM = np.zeros((JKeM,size_phi),complex)
 
 
 	#block for construction of individual basis begins 
@@ -145,46 +153,63 @@ if __name__ == '__main__':
 		for th in range(len(xGL)):
 			dJKeM[s,th] = np.sqrt((2.*JKeMQuantumNumList[s,0]+1)/(8.*np.pi**2))*littleD(JKeMQuantumNumList[s,0],JKeMQuantumNumList[s,2],JKeMQuantumNumList[s,1],np.arccos(xGL[th]))*np.sqrt(wGL[th])
 
-		for ph in range(angleNum):
+		for ph in range(size_phi):
 			KJKeM[s,ph] = np.exp(1j*phixiGridPts[ph]*JKeMQuantumNumList[s,1])*np.sqrt(dphixi)*Nk
 			MJKeM[s,ph] = np.exp(1j*phixiGridPts[ph]*JKeMQuantumNumList[s,2])*np.sqrt(dphixi)*Nk
 	#block for construction of individual basis ends
 
 	#block for construction of |J1K1M1,J2K2M2> basis begins 
 	eEEbasisuse = KJKeM[:,np.newaxis,np.newaxis,:]*MJKeM[:,np.newaxis,:,np.newaxis]*dJKeM[:,:,np.newaxis,np.newaxis]
-	eEEebasisuse = np.reshape(eEEbasisuse,(JKeM,len(xGL)*angleNum*angleNum),order='C')
+	eEEebasisuse = np.reshape(eEEbasisuse,(JKeM,size_theta*size_phi*size_phi),order='C')
 	#block for construction of |J1K1M1,J2K2M2> basis ends
 
 	#printing block is opened
 	normMat = np.tensordot(eEEebasisuse, np.conjugate(eEEebasisuse), axes=([1],[1]))
-	norm_check_file = "norm-check"+strFile
-	norm_check_write = open("exact-energies-of-H2O/"+norm_check_file,'w')
-	norm_check_write.write("eEEbasisuse.shape: shape of the even |JKM> basis: " + str(eEEbasisuse.shape)+" \n")
-	norm_check_write.write("eEEebasisuse.shape: reduced shape of the even |JKM> basis: " + str(eEEebasisuse.shape)+" \n")
-	norm_check_write.write("normMat.shape: shape of the even <JKM|JKM> basis: " + str(normMat.shape)+" \n")
-	norm_check_write.write("\n")
-	norm_check_write.write("\n")
+	if (norm_check == True):
+		norm_check_file = "../exact-energies-of-H2O/norm-check-"+strFile
+		norm_check_write = open(norm_check_file,'w')
+		norm_check_write.write("eEEbasisuse.shape: shape of the even |JKM> basis: " + str(eEEbasisuse.shape)+" \n")
+		norm_check_write.write("eEEebasisuse.shape: reduced shape of the even |JKM> basis: " + str(eEEebasisuse.shape)+" \n")
+		norm_check_write.write("normMat.shape: shape of the even <JKM|JKM> basis: " + str(normMat.shape)+" \n")
+		norm_check_write.write("\n")
+		norm_check_write.write("\n")
 
-	for s1 in range(JKeM):
-		for s2 in range(JKeM):
-			if (np.abs(normMat[s1,s2]) > tol):
-				norm_check_write.write("L vec Rotor1: "+str(JKeMQuantumNumList[s1,0])+" "+str(JKeMQuantumNumList[s1,1])+" "+str(JKeMQuantumNumList[s1,2])+"\n")
-				norm_check_write.write("R vec Rotor1: "+str(JKeMQuantumNumList[s2,0])+" "+str(JKeMQuantumNumList[s2,1])+" "+str(JKeMQuantumNumList[s2,2])+"\n")
-				norm_check_write.write("Norm: "+str(normMat[s1,s2])+"\n")
-				norm_check_write.write("\n")
-	norm_check_write.close()
-	#printing block is closed
+		for s1 in range(JKeM):
+			for s2 in range(JKeM):
+				if (np.abs(normMat[s1,s2]) > tol):
+					norm_check_write.write("L vec Rotor1: "+str(JKeMQuantumNumList[s1,0])+" "+str(JKeMQuantumNumList[s1,1])+" "+str(JKeMQuantumNumList[s1,2])+"\n")
+					norm_check_write.write("R vec Rotor1: "+str(JKeMQuantumNumList[s2,0])+" "+str(JKeMQuantumNumList[s2,1])+" "+str(JKeMQuantumNumList[s2,2])+"\n")
+					norm_check_write.write("Norm: "+str(normMat[s1,s2])+"\n")
+					norm_check_write.write("\n")
+		norm_check_write.close()
+		#printing block is closed
 
 
 	#Computation of rotational energy of a asymmetric top molecule
 	#Construction of potential matrix begins
-	v1d = np.zeros(len(xGL)*angleNum*angleNum,float)
+	'''
+	v1d = np.zeros(size_theta*size_phi*size_phi,float)
 	ii = 0
-	for th1 in range(len(xGL)):
-		for ph1 in range(angleNum):
-			for ch1 in range(angleNum):
-				v1d[ii]=-strength*xGL[th1]
+	for th1 in range(size_theta):
+		for ph1 in range(size_phi):
+			for ch1 in range(size_phi):
+				v1d[ii]=-zCOM*xGL[th1]
+				v1d[ii]=0.0#-zCOM*xGL[th1]
 				ii = ii + 1
+	'''
+	#Construction of potential matrix ends
+	#Construction of potential matrix begins
+	com1=[0.0,0.0,0.0]
+	com2=[0.0,0.0,zCOM]
+	Eulang2=[0.0, 0.0, 0.0]
+	#Eulang2=[0.0, math.pi, 0.0]
+	v1d = np.zeros(size_theta*size_phi*size_phi,float)
+	for th1 in range(size_theta):
+		for ph1 in range(size_phi):
+			for ch1 in range(size_phi):
+				ii = ch1+(ph1+th1*size_phi)*size_phi
+				Eulang1=[phixiGridPts[ph1], math.acos(xGL[th1]), phixiGridPts[ch1]]
+				v1d[ii]=qpot.caleng(com1,com2,Eulang1,Eulang2)
 	#Construction of potential matrix ends
 
 	#Construction of a constant potential matrix over the three Euler angles
@@ -192,27 +217,28 @@ if __name__ == '__main__':
 	HpotKe = np.tensordot(np.conjugate(eEEebasisuse), tempa, axes=([1],[1]))
 
 	#printing block is opened
-	pot_check_file = "pot-check"+strFile
-	pot_check_write = open("exact-energies-of-H2O/"+pot_check_file,'w')
-	pot_check_write.write("Printing of shapes and elements of potential matrix - "+"\n")
-	pot_check_write.write("\n")
-	pot_check_write.write("\n")
-	pot_check_write.write("shape of potential matrix over three Euler angles : " + str(v1d.shape)+" \n")
-	pot_check_write.write("eEEebasisuse.shape: reduced shape of the even |JKM> basis: " + str(eEEebasisuse.shape)+" \n")
-	pot_check_write.write("shape of HpotKe : " + str(HpotKe.shape)+" \n")
-	pot_check_write.write("\n")
-	pot_check_write.write("\n")
+	if (pot_write == True):
+		pot_check_file = "../exact-energies-of-H2O/pot-check-"+strFile
+		pot_check_write = open(pot_check_file,'w')
+		pot_check_write.write("Printing of shapes and elements of potential matrix - "+"\n")
+		pot_check_write.write("\n")
+		pot_check_write.write("\n")
+		pot_check_write.write("shape of potential matrix over three Euler angles : " + str(v1d.shape)+" \n")
+		pot_check_write.write("eEEebasisuse.shape: reduced shape of the even |JKM> basis: " + str(eEEebasisuse.shape)+" \n")
+		pot_check_write.write("shape of HpotKe : " + str(HpotKe.shape)+" \n")
+		pot_check_write.write("\n")
+		pot_check_write.write("\n")
 
-	if (write_pot == True):
-		for s1 in range(JKeM):
-			for s2 in range(JKeM):
-				if (np.abs(HpotKe[s1,s2]) > tol):
-					pot_check_write.write("L vec Rotor1: "+str(JKeMQuantumNumList[s1,0])+" "+str(JKeMQuantumNumList[s1,1])+" "+str(JKeMQuantumNumList[s1,2])+"\n")
-					pot_check_write.write("R vec Rotor1: "+str(JKeMQuantumNumList[s2,0])+" "+str(JKeMQuantumNumList[s2,1])+" "+str(JKeMQuantumNumList[s2,2])+"\n")
-					pot_check_write.write("Constant potential field - Re: "+str(np.real(HpotKe[s1,s2]))+"   Im: "+str(np.imag(HpotKe[s1,s2]))+"\n")
-					pot_check_write.write("\n")
-	pot_check_write.close()
-	# printing block is closed
+		if (potwrite == True):
+			for s1 in range(JKeM):
+				for s2 in range(JKeM):
+					if (np.abs(HpotKe[s1,s2]) > tol):
+						pot_check_write.write("L vec Rotor1: "+str(JKeMQuantumNumList[s1,0])+" "+str(JKeMQuantumNumList[s1,1])+" "+str(JKeMQuantumNumList[s1,2])+"\n")
+						pot_check_write.write("R vec Rotor1: "+str(JKeMQuantumNumList[s2,0])+" "+str(JKeMQuantumNumList[s2,1])+" "+str(JKeMQuantumNumList[s2,2])+"\n")
+						pot_check_write.write("Constant potential field - Re: "+str(np.real(HpotKe[s1,s2]))+"   Im: "+str(np.imag(HpotKe[s1,s2]))+"\n")
+						pot_check_write.write("\n")
+		pot_check_write.close()
+		# printing block is closed
 
 
 	# construction of kinetic energy matrix - BEGINS
@@ -244,8 +270,8 @@ if __name__ == '__main__':
 	#printing block is opened
 	eigValKe_comb = np.array([eigValKe_sort, eigValKe_sort/CMRECIP2KL])
 
-	eigValKe_file = "eigen-values"+strFile
-	np.savetxt("exact-energies-of-H2O/"+eigValKe_file, eigValKe_comb.T, fmt='%20.8f', delimiter=' ', header='Energy levels of a aymmetric top - Units associated with the first and second columns are Kelvin and wavenumber, respectively. ')
+	eigValKe_file = "../exact-energies-of-H2O/eigen-values-"+strFile
+	np.savetxt(eigValKe_file, eigValKe_comb.T, fmt='%20.8f', delimiter=' ', header='Energy levels of a aymmetric top - Units associated with the first and second columns are Kelvin and wavenumber, respectively. ')
 
 	for idx in range(4):
 		eigVecKeRe = np.real(np.dot(np.conjugate(eigVecKe_sort[:,idx].T),eigVecKe_sort[:,idx]))
@@ -262,12 +288,13 @@ if __name__ == '__main__':
 	avgHpotKeL = np.dot(HpotKe,eigVecKe_sort[:,idx])
 	avgHpotKe = np.dot(np.conjugate(eigVecKe_sort[:,idx].T),avgHpotKeL)
 
-	gs_eng_file = "ground-state-energies"+strFile
-	gs_eng_write = open("exact-energies-of-H2O/"+gs_eng_file,'w')
+	gs_eng_file = "../exact-energies-of-H2O/ground-state-energies-"+strFile
+	gs_eng_write = open(gs_eng_file,'w')
 	gs_eng_write.write("#Printing of ground state energies in inverse Kelvin - "+"\n")
 	gs_eng_write.write('{0:1} {1:^19} {2:^20}'.format("#","<T+V>", "<V>"))
 	gs_eng_write.write("\n")
 	gs_eng_write.write('{0:^20.8f} {1:^20.8f}'.format(eigValKe_sort[0], avgHpotKe.real))
+	gs_eng_write.write("\n")
 	gs_eng_write.close()
 	# printing block is closed
 
--- nonlinear-rotors/codes-diagonalization/submission-diag.py
+++ nonlinear-rotors/codes-diagonalization/submission-diag.py
@@ -5,11 +5,11 @@ import os
 import decimal
 import numpy as np
  
-def jobstring(NameOfServer, src_code, Rpt, jmax, size_grid, dir_output):
-	logfile = dir_output+"/diag-submit-p-H2O-Rpt"+str(Rpt)+"ang-j"+str(jmax)+"-grid"+str(size_grid)+".txt"
-	jobname = "diag-Rpt"+str(Rpt)
+def jobstring(NameOfServer, src_code, Rpt, jmax, dir_output):
+	logfile = dir_output+"/diag-submit-p-H2O-Rpt"+str(Rpt)+"ang-j"+str(jmax)+".txt"
+	jobname = "dg-R"+str(Rpt)
 
-	command_execution = "python " + src_code +"  " +  str(Rpt) + "  " + str(jmax) + "  " + str(size_grid)
+	command_execution = "python " + src_code +"  " +  str(Rpt) + "  " + str(jmax)
 
 	if NameOfServer == "graham":
 		account = "#SBATCH --account=rrg-pnroy"
@@ -21,7 +21,7 @@ def jobstring(NameOfServer, src_code, Rpt, jmax, size_grid, dir_output):
 #SBATCH --output=%s
 #SBATCH --time=1-00:00
 %s
-#SBATCH --mem-per-cpu=256GB
+#SBATCH --mem-per-cpu=32GB
 #SBATCH --cpus-per-task=1
 export OMP_NUM_THREADS=1
 %s
@@ -30,77 +30,107 @@ export OMP_NUM_THREADS=1
 
 #initial parameters for qmc.input
 status = 'S'
-grid_increment = 0
-jrot = 2
-zmin = 10.0
-zmax = 10.0
+jrot = 16
+
+# making grid points for the intermolecular distance, r
+zmin = 2.5
+zmax = 2.7
+dz = 0.02
+nz = int(((zmax-zmin)+dz*0.5)/dz)
+nz += 1
+zList = [zmin+dz*i for i in range(nz)]
+zList += [2.75]
+zmin = 2.8
+zmax = 5.0
 dz = 0.1
 nz = int(((zmax-zmin)+dz*0.5)/dz)
-nz=nz+1
+nz += 1
+zList += [zmin+dz*i for i in range(nz)]
+zmin = 5.2
+zmax = 10.0
+dz = 0.2
+nz = int(((zmax-zmin)+dz*0.5)/dz)
+nz += 1
+print(nz)
+zList += [zmin+dz*i for i in range(nz)]
 
 NameOfServer = 'nlogn'
 dir_output = "/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O"
 
 if (status == 'S'):
 	src_dir = os.getcwd()
-	src_code = "/diag_dimer_asym_rot_basis_saved.py"
+	src_code = "/diag_monomer_asym_rot.py"
 	run_command = src_dir + src_code
 	print(run_command)
 
 if (status == 'A'):
-	eigvalvsRpt1 = np.zeros(nz)
-	eigvalvsRpt2 = np.zeros(nz)
-	saved_Rpt = np.zeros(nz)
-	SvNvsRpt = np.zeros(nz)
-	S2vsRpt = np.zeros(nz)
+	eigvalvsRpt1 = np.zeros(len(zList),dtype=float)
+	eigvalvsRpt2 = np.zeros(len(zList),dtype=float)
+	saved_Rpt = np.zeros(len(zList),dtype=float)
+	SvNvsRpt = np.zeros(len(zList),dtype=float)
+	S2vsRpt = np.zeros(len(zList),dtype=float)
 
 # Loop over your jobs
-for i in range(nz): 
-
-	value = zmin+i*dz
-	Rpt = '{:2.1f}'.format(value)
-	print(Rpt)
+index_end=0
+for r in zList: 
+	Rpt = '{:3.2f}'.format(r)
 
 	if (status == 'S'):
 		os.chdir(dir_output)
 		#job submission
-		fname = 'diag_Rpt'+str(Rpt)+"Ang"
-		fwrite = open(fname, 'w')
-
-		fwrite.write(jobstring(NameOfServer, run_command, Rpt, jrot, grid_increment, dir_output))
-		fwrite.close()
-		call(["sbatch", "-p", "highmem", fname])
+		fname = 'job-diag-R'+str(Rpt)+"Ang-Jmax"+str(jrot)
+		if (os.path.isfile(fname)==True):
+			print("job name - "+fname)
+			print("It has already been submitted.")
+			os.chdir(src_dir)
+			exit()
+		else:
+			fwrite=open(fname, 'w')
+			fwrite.write(jobstring(NameOfServer, run_command, Rpt, jrot, dir_output))
+			fwrite.close()
+			call(["sbatch", "-p", "highmem", fname])
+			#call(["sbatch", "-C", "faster", fname])
 
 		os.chdir(src_dir)
 
 	if (status == "A"):
-		saved_Rpt[i] = Rpt
-		thetaNum = int(2*jrot+3+grid_increment)
-		angleNum = int(2*(2*jrot+1)+grid_increment)
-		strFile = "diag-2-p-H2O-jmax"+str(jrot)+"-Rpt"+str(Rpt)+"Angstrom-grid-"+str(thetaNum)+"-"+str(angleNum)+"-saved-basis.txt"
+		saved_Rpt[index_end] = float(Rpt)
+		thetaNum = int(2*jrot+3)
+		angleNum = int(2*(2*jrot+1))
+			
+		strFile = "diag-2-p-H2O-one-rotor-fixed-cost1-jmax"+str(jrot)+"-Rpt"+str(Rpt)+"Angstrom-grids-"+str(thetaNum)+"-"+str(angleNum)+"-saved-basis.txt"
 
-		fileAnalyze_energy = "ground-state-energy-"+strFile
+		fileAnalyze_energy = "ground-state-energies-"+strFile
 		data_input_energy = dir_output+"/"+fileAnalyze_energy
-		eig_kelvin, eig_wavenumber = np.loadtxt(data_input_energy, usecols=(0, 1), unpack=True)
-		eigvalvsRpt1[i] = eig_kelvin[0]
-		eigvalvsRpt2[i] = eig_wavenumber[0]
+		CMRECIP2KL = 1.4387672
 
 		fileAnalyze_entropy = "ground-state-entropies-"+strFile
 		data_input_entropy = dir_output+"/"+fileAnalyze_entropy
-		ent = np.loadtxt(data_input_entropy, usecols=(0), unpack=True)
-		SvNvsRpt[i] = ent[0]
-		S2vsRpt[i] = ent[1]
+
+		#print(data_input_energy)
+		if (os.path.isfile(data_input_energy) == True):
+			eig_kelvin, eig_wavenumber = np.loadtxt(data_input_energy, usecols=(0, 1), unpack=True)
+			#print(eig_kelvin, eig_wavenumber)
+			eigvalvsRpt1[index_end] = eig_kelvin
+			eigvalvsRpt2[index_end] = eig_wavenumber
+		else:
+			pass
+
+		print(index_end)
+		index_end = index_end+1
 
 if (status == "A"):
 	#printing block is opened
-	strFile1 = "diag-2-p-H2O-jmax"+str(jrot)+"-grid-"+str(thetaNum)+"-"+str(angleNum)+"-saved-basis.txt"
+	strFile1 = "diag-2-p-H2O-one-rotor-fixed-cost1-jmax"+str(jrot)+"-grid-"+str(thetaNum)+"-"+str(angleNum)+"-saved-basis.txt"
 
 	energy_comb = np.array([saved_Rpt, eigvalvsRpt1, eigvalvsRpt2])
 	eig_file = dir_output+"/ground-state-energy-vs-Rpt-"+strFile1
 	np.savetxt(eig_file, energy_comb.T, fmt='%20.8f', delimiter=' ', header='First col. --> Rpt (Angstrom); 2nd and 3rd cols are the eigen values of (Htot = Hrot + Hvpot) in Kelvin and wavenumber, respectively. ')
 
+	'''
 	entropy_comb = np.array([saved_Rpt, SvNvsRpt, S2vsRpt])
 	ent_file = dir_output+"/ground-state-entropies-vs-Rpt-"+strFile1
 	np.savetxt(ent_file, entropy_comb.T, fmt='%20.8f', delimiter=' ', header='First col. --> Rpt (Angstrom); 2nd and 3rd cols are the S_vN and S_2, respectively. ')
 	# printing block is closed
+	'''
 
