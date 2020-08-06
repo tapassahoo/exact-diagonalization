c49f030af543acd3316988dd5187d9395ee15b44
Tapas Sahoo <tapascuchem@gmail.com>
2020-08-06 00:42:32 -0400

    rsync script is added

---
+++ nonlinear-rotors/codes-diagonalization/push-to-graham.sh
@@ -0,0 +1,1 @@
+rsync -avzhe ssh /home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-theta-distribution-diag-2-p-H2O-one-rotor-fixed-cost1* tapas@graham.computecanada.ca:/home/tapas/ResultsOfExact/
